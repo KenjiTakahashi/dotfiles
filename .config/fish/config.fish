6eb7792d6f04fec9f1dd8408ca09db9a1377a5ca
KenjiTakahashi <wozniakk@gmail.com>
2021-02-14 12:32:29 +0100

    zion init

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 fish_vi_key_bindings
 
-set -l vi_mode_icon '*'
+set -l vi_mode_icon 'λ'
 set -g tide_vi_mode_default_icon $vi_mode_icon
 set -g tide_vi_mode_default_color blue
 set -g tide_vi_mode_insert_icon $vi_mode_icon
